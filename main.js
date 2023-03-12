342af7714a9943de0cf0c502b780d94f19dbd184
Yana0001 <119385481+Yana0001@users.noreply.github.com>
2023-03-12 19:15:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const list = document.querySelector(".list");
+const listInput = document.querySelector(".list__input");
+const saveBtn = document.querySelector(".list__btn");
+
+saveBtn.addEventListener("submit", function () {
+  console.log("SUBMIT");
+});
