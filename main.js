8cda96f7576ae740e573b30a64d8d90bd821e7e0
Yana0001 <119385481+Yana0001@users.noreply.github.com>
2023-03-13 02:08:32 +0200

    add tasks js

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
-const list = document.querySelector(".list");
-const listInput = document.querySelector(".list__input");
-const saveBtn = document.querySelector(".list__btn");
+const submitBtn = document.querySelector(".list__btn");
+const taskInput = document.querySelector("#task-input");
+const tasksList = document.querySelector("#tasksList");
 
-saveBtn.addEventListener("submit", function () {
-  console.log("SUBMIT");
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const taskText = taskInput.value;
+  console.log(taskText);
+
+  const taskHTML = `<div class="input-wrapper">
+              <input
+                class="list__task__item-text"
+                type="text"
+                value='${taskText}'
+              />
+              <ul class="list__wrapper">
+                <li class="li__item-first">
+                  <button class="btn__f btn" type="button">
+                    <svg class="list__task__svg-trash" width="20" height="20">
+                      <use href="./img/symbol-defs.svg#trash"></use>
+                    </svg>
+                  </button>
+                </li>
+                <li class="li__item-second">
+                  <button class="btn__s btn" type="button">
+                    <svg class="list__task__svg-pencil" width="20" height="20">
+                      <use href="./img/symbol-defs.svg#pencil"></use>
+                    </svg>
+                  </button>
+                </li>
+                <li class="li__item-third">
+                  <button class="btn__th btn" type="button">
+                    <svg class="list__task__svg-check" width="20" height="20">
+                      <use href="./img/symbol-defs.svg#check-circle"></use>
+                    </svg>
+                  </button>
+                </li>
+              </ul>
+            </div>`;
+
+  tasksList.insertAdjacentHTML("beforeend", taskHTML);
+
+  taskInput.value = "";
+  taskInput.focus();
 });
