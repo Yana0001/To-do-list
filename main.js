3ee413086afccfce3d82528354e14433b748c564
Yana0001 <119385481+Yana0001@users.noreply.github.com>
2023-03-15 00:35:16 +0200

    add js to btns and list

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const submitBtn = document.querySelector(".list__btn");
 const taskInput = document.querySelector("#task-input");
 const tasksList = document.querySelector("#tasksList");
+const emptyList = document.querySelector("#emptyList");
 
+// add task
 submitBtn.addEventListener("click", addTask);
 
+// delete task
 tasksList.addEventListener("click", deleteTask);
 
+// done task
+tasksList.addEventListener("click", doneTask);
+
+// functions
 function addTask(event) {
   event.preventDefault();
 
@@ -16,14 +23,14 @@ function addTask(event) {
               <li
                 class="list-group-item d-flex justify-content-between task-item"
               >
-                <span class="task-title">Купити молоко</span>
+                <span class="task-title task-title--done">${taskText}</span>
                 <div class="task-item__buttons">
-                  <button type="button" data-action="done" class="btn-action">
+                  <button type="button" data-action="delete" class="btn-action">
                     <svg class="btn-trash" width="20" height="20">
                       <use href="./img/symbol-defs.svg#trash"></use>
                     </svg>
                   </button>
-                  <button type="button" data-action="delete" class="btn-action">
+                  <button type="button" data-action="done" class="btn-action">
                     <svg class="btn-check" width="20" height="20">
                       <use href="./img/symbol-defs.svg#check-circle"></use>
                     </svg>
@@ -38,5 +45,17 @@ function addTask(event) {
 }
 
 function deleteTask(event) {
-  console.log(event.target);
+  if (event.target.dataset.action === "delete") {
+    const parentNode = event.target.closest(".list-group-item");
+    parentNode.remove();
+  }
+}
+
+function doneTask(event) {
+  if (event.target.dataset.action === "done") {
+    const parentNode = event.target.closest(".list-group-item");
+    const taskTitle = parentNode.querySelector(".task-title");
+    taskTitle.classList.toggle("task-title--done");
+    console.log(taskTitle);
+  }
 }
